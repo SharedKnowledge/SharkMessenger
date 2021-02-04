0dbe37bfcb16c32796086512b675de9b1c5ad591
thsc42 <thomas.schwotzer@htw-berlin.de>
2021-02-04 08:48:30 +0100

    start working.

--- src/net/sharksystem/messenger/SharkMessengerComponent.java
+++ src/net/sharksystem/messenger/SharkMessengerComponent.java
@@ -3,9 +3,17 @@ package net.sharksystem.messenger;
 import net.sharksystem.ASAPFormats;
 import net.sharksystem.SharkComponent;
 
+/**
+ * A decentralized messenger using ASAP - it does not require Internet access. It can take any
+ * network, as unreliable as it might be, to transfer data.
+ *
+ * Message exchange is (optionally) signed and encrypted. It makes use of the ASAPCertificateExchange component.
+ * It is essential part and first demonstrator of an Android Shark application.
+ */
 @ASAPFormats(formats = {SharkMessengerComponent.SHARK_MESSENGER_FORMAT})
 public interface SharkMessengerComponent extends SharkComponent {
     String SHARK_MESSENGER_FORMAT = "shark/messenger";
+
 
 
 }
