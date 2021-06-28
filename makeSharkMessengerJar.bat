5bde28159e59e52e8b0ef245ebf4a910e9529f6a
boris.foko <fokoboris@gmail.com>
2021-06-28 04:10:05 +0200

    add SortedMessage implementation and SortedSharkMessage Factory to SharkMessage component

--- makeSharkMessengerJar.bat
+++ makeSharkMessengerJar.bat
@@ -1,3 +1,15 @@
-cd out/production/SharkMessenger/
+@ECHO off
+
+SET outProdDir="out/production/SharkMessenger/"
+ 
+@rem Check if the destination Directory exist
+IF not exist outProdDir (
+	mkdir %outProdDir%
+)
+
+CD %outProdDir%
 jar -cf SharkMessenger.jar net
-move SharkMessenger.jar ../../../SharkMessenger.jar
+MOVE SharkMessenger.jar ../../../SharkMessenger.jar
+
+@rem move back to the root of the project
+CD ../../..
--- tests/net/sharksystem/messenger/MessageExchangeTests.java
+++ tests/net/sharksystem/messenger/MessageExchangeTests.java
@@ -11,7 +11,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Version 1 - Scenarios (open channel communication (open routing) only)  selective routing is Version 2
@@ -27,6 +29,10 @@ import java.util.List;
  * Clara identity, Alice has no clue.
  * vi) Alice sends two signed and encrypted messages to C and B with success.
  * vii) Alice sends signed B. B encounters C. B can verify, C can not. Like ii) but routed over B.
+ * Viii) Sorted message handling between Alice and Bob without timestamp. Alice send message bob receive,
+ * and send an other one, and then send a replyTo message
+ * iX) Sorted message handling between Alice, Bob, Clara and Tina without timestamp. Alice send message bob receive,
+ *  * and send an other one, and then send a replyTo message
  */
 
 public class MessageExchangeTests extends TestHelper {
@@ -262,5 +268,297 @@ public class MessageExchangeTests extends TestHelper {
         SharkMessageList aliceChannelMessages = aliceChannel.getMessages(false, false);
         Assert.assertEquals(2, aliceChannelMessages.size());
         this.oneEncryptableOneIsNot(aliceChannelMessages);
+    }
+
+    @Test
+    public void test1_8() throws ASAPSecurityException, SharkException, IOException, InterruptedException {
+        this.setUpScenario_1();
+
+        SortedMessageFactory aliceSortedMessageFactory = new SortedSharkMessageFactory();
+        SortedMessageFactory bobSortedMessageFactory = new SortedSharkMessageFactory();
+        SortedMessage aliceSortedMessage = aliceSortedMessageFactory.produceSortedMessage(MESSAGE_BYTE, null);
+        // Alice send a message to Bob
+        aliceMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(aliceSortedMessage) , URI, bobPeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Alice - Bob ////////////////////////////////////////////////////
+        this.runEncounter(this.alicePeer, this.bobPeer, true);
+
+        // Test results Bob received message
+        SharkMessengerChannel bobChannel = bobMessenger.getChannel(URI);
+        SharkMessage bobSharkMessage = bobChannel.getMessages().getSharkMessage(0, true);
+        Assert.assertTrue(bobSharkMessage.couldBeDecrypted());
+        Assert.assertTrue(bobSharkMessage.encrypted());
+        Assert.assertTrue(bobSharkMessage.verified());
+        // message received by Bob from Alice?
+        Assert.assertTrue(alicePeer.samePeer(bobSharkMessage.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage bobSortedMessage = SortedMessageImpl.byteArrayToSortedMessage(bobSharkMessage.getContent());
+        // Add the received message to the factory
+        bobSortedMessageFactory.addIncomingSortedMessage(bobSortedMessage);
+        // Check sortedMessage id
+        Assert.assertEquals(aliceSortedMessage.getID(), bobSortedMessage.getID());
+
+        Assert.assertTrue(Utils.compareArrays(bobSortedMessage.getContent(), MESSAGE_BYTE));
+
+        // SortedMessage parents should be []
+        Assert.assertTrue(bobSortedMessage.getParents().size() == 0);
+
+        // Bob send a message to Alice
+        SortedMessage bobSortedMessage_1 = bobSortedMessageFactory.produceSortedMessage(MESSAGE_1_BYTE, null);
+        bobMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(bobSortedMessage_1) , URI, alicePeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Bob - Alice ////////////////////////////////////////////////////
+        this.runEncounter(this.bobPeer, this.alicePeer, true);
+
+        // Test results Bob received message
+        SharkMessengerChannel aliceChannel = aliceMessenger.getChannel(URI);
+        SharkMessage aliceSharkMessage_1 = aliceChannel.getMessages().getSharkMessage(0, true);
+        Assert.assertTrue(aliceSharkMessage_1.couldBeDecrypted());
+        Assert.assertTrue(aliceSharkMessage_1.encrypted());
+        Assert.assertTrue(aliceSharkMessage_1.verified());
+        // message received by Alice from Bob?
+        Assert.assertTrue(bobPeer.samePeer(aliceSharkMessage_1.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage aliceSortedMessage_1 = SortedMessageImpl.byteArrayToSortedMessage(aliceSharkMessage_1.getContent());
+        // Add the received message to the factory
+        aliceSortedMessageFactory.addIncomingSortedMessage(bobSortedMessage_1);
+        // Check sortedMessage id
+        Assert.assertEquals(bobSortedMessage_1.getID(), aliceSortedMessage_1.getID());
+
+        Assert.assertTrue(Utils.compareArrays(aliceSortedMessage_1.getContent(), MESSAGE_1_BYTE));
+
+        // SortedMessage parents should be ["id"] bobSortedMessage.getID()
+        Assert.assertTrue(aliceSortedMessage_1.getParents().size() == 1);
+        Assert.assertTrue(aliceSortedMessage_1.getParents().contains(bobSortedMessage.getID()));
+
+        // Bob send a message to Alice with replyTo relation to bobSortedMessage_1
+        SortedMessage bobSortedMessage_2 = bobSortedMessageFactory.produceSortedMessage(MESSAGE_2_BYTE, bobSortedMessage.getID());
+        bobMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(bobSortedMessage_2) , URI, alicePeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Bob - Alice ////////////////////////////////////////////////////
+        this.runEncounter(this.bobPeer, this.alicePeer, true);
+
+        // Test results Bob received message
+        SharkMessage aliceSharkMessage_2 = aliceChannel.getMessages().getSharkMessage(1, true);
+        Assert.assertTrue(aliceSharkMessage_2.couldBeDecrypted());
+        Assert.assertTrue(aliceSharkMessage_2.encrypted());
+        Assert.assertTrue(aliceSharkMessage_2.verified());
+        // message received by Alice from Bob?
+        Assert.assertTrue(bobPeer.samePeer(aliceSharkMessage_2.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage aliceSortedMessage_2 = SortedMessageImpl.byteArrayToSortedMessage(aliceSharkMessage_2.getContent());
+        // Add the received message to the factory
+        aliceSortedMessageFactory.addIncomingSortedMessage(aliceSortedMessage_2);
+        // Check sortedMessage id
+        Assert.assertEquals(bobSortedMessage_2.getID(), aliceSortedMessage_2.getID());
+
+        Assert.assertTrue(Utils.compareArrays(aliceSortedMessage_2.getContent(), MESSAGE_2_BYTE));
+
+        // SortedMessage parents should be ["id"] bobSortedMessage_1.getID()
+        Assert.assertTrue(bobSortedMessage_2.getParents().size() == 1);
+        Assert.assertTrue(bobSortedMessage_2.getParents().contains(aliceSortedMessage_1.getID()));
+
+        // SortedMessage reply To should be bobSortedMessage.getID()
+        Assert.assertEquals(bobSortedMessage_2.getReplyTo(), bobSortedMessage.getID());
+    }
+
+    @Test
+    public void test1_9() throws ASAPSecurityException, SharkException, IOException, InterruptedException {
+        this.setUpScenario_2();
+
+        SortedMessageFactory aliceSortedMessageFactory = new SortedSharkMessageFactory();
+        SortedMessageFactory bobSortedMessageFactory = new SortedSharkMessageFactory();
+        SortedMessageFactory claraSortedMessageFactory = new SortedSharkMessageFactory();
+        SortedMessageFactory davidSortedMessageFactory = new SortedSharkMessageFactory();
+
+        // Alice send a message to Bob
+        SortedMessage aliceSortedMessage = aliceSortedMessageFactory.produceSortedMessage(MESSAGE_BYTE, null);
+        aliceMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(aliceSortedMessage) , URI, bobPeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Alice - Bob ////////////////////////////////////////////////////
+        this.runEncounter(this.alicePeer, this.bobPeer, true);
+
+        // Test results Bob received message
+        SharkMessengerChannel bobChannel = bobMessenger.getChannel(URI);
+        SharkMessage bobSharkMessage = bobChannel.getMessages().getSharkMessage(0, true);
+        Assert.assertTrue(bobSharkMessage.couldBeDecrypted());
+        Assert.assertTrue(bobSharkMessage.encrypted());
+        Assert.assertTrue(bobSharkMessage.verified());
+        // message received by Bob from Alice?
+        Assert.assertTrue(alicePeer.samePeer(bobSharkMessage.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage bobSortedMessage = SortedMessageImpl.byteArrayToSortedMessage(bobSharkMessage.getContent());
+        // Add the received message to the factory
+        bobSortedMessageFactory.addIncomingSortedMessage(bobSortedMessage);
+        // Check sortedMessage id
+        Assert.assertEquals(aliceSortedMessage.getID(), bobSortedMessage.getID());
+
+        Assert.assertTrue(Utils.compareArrays(bobSortedMessage.getContent(), MESSAGE_BYTE));
+
+        // SortedMessage parents should be []
+        Assert.assertEquals(0, bobSortedMessage.getParents().size());
+
+        // Alice send a message to Clara
+        aliceMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(aliceSortedMessage) , URI, claraPeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Alice - Clara ////////////////////////////////////////////////////
+        this.runEncounter(this.alicePeer, this.claraPeer, true);
+
+        // Test results clara received message
+        SharkMessengerChannel claraChannel = bobMessenger.getChannel(URI);
+        SharkMessage claraSharkMessage = claraChannel.getMessages().getSharkMessage(0, true);
+        Assert.assertTrue(claraSharkMessage.couldBeDecrypted());
+        Assert.assertTrue(claraSharkMessage.encrypted());
+        Assert.assertTrue(claraSharkMessage.verified());
+        // message received by Bob from Alice?
+        Assert.assertTrue(alicePeer.samePeer(claraSharkMessage.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage claraSortedMessage = SortedMessageImpl.byteArrayToSortedMessage(claraSharkMessage.getContent());
+        // Add the received message to the factory
+        claraSortedMessageFactory.addIncomingSortedMessage(claraSortedMessage);
+        // Check sortedMessage id
+        Assert.assertEquals(aliceSortedMessage.getID(), claraSortedMessage.getID());
+
+        Assert.assertTrue(Utils.compareArrays(claraSortedMessage.getContent(), MESSAGE_BYTE));
+
+        // SortedMessage parents should be []
+        Assert.assertEquals(0, claraSortedMessage.getParents().size());
+
+        // Bob send message to Alice
+        SortedMessage bobSortedMessage_1 = bobSortedMessageFactory.produceSortedMessage(MESSAGE_1_BYTE, aliceSortedMessage.getID());
+        bobMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(bobSortedMessage_1) , URI, alicePeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Bob - Alice ////////////////////////////////////////////////////
+        this.runEncounter(this.bobPeer, this.alicePeer, true);
+
+        // Test results Alice received message
+        SharkMessengerChannel aliceChannel = aliceMessenger.getChannel(URI);
+        SharkMessage aliceSharkMessage_1 = aliceChannel.getMessages().getSharkMessage(0, true);
+        Assert.assertTrue(aliceSharkMessage_1.couldBeDecrypted());
+        Assert.assertTrue(aliceSharkMessage_1.encrypted());
+        Assert.assertTrue(aliceSharkMessage_1.verified());
+        // message received by Alice from Bob?
+        Assert.assertTrue(bobPeer.samePeer(aliceSharkMessage_1.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage aliceSortedMessage_1 = SortedMessageImpl.byteArrayToSortedMessage(aliceSharkMessage_1.getContent());
+        // Add the received message to the factory
+        aliceSortedMessageFactory.addIncomingSortedMessage(aliceSortedMessage_1);
+        // Check sortedMessage id
+        Assert.assertEquals(bobSortedMessage_1.getID(), aliceSortedMessage_1.getID());
+
+        Assert.assertTrue(Utils.compareArrays(aliceSortedMessage_1.getContent(), MESSAGE_1_BYTE));
+
+        // SortedMessage parents should be ["id"] bobSortedMessage.getID()
+        Assert.assertEquals(1, aliceSortedMessage_1.getParents().size());
+        Assert.assertTrue(aliceSortedMessage_1.getParents().contains(bobSortedMessage.getID()));
+
+        // Clara send message to Alice
+        SortedMessage claraSortedMessage_1 = claraSortedMessageFactory.produceSortedMessage(MESSAGE_BYTE, aliceSortedMessage.getID());
+        claraMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(claraSortedMessage_1) , URI, alicePeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Clara - Alice ////////////////////////////////////////////////////
+        this.runEncounter(this.claraPeer, this.alicePeer, true);
+
+        // Test results Alice received message
+        SharkMessage aliceSharkMessage_11 = aliceChannel.getMessages().getSharkMessage(0, true);
+        Assert.assertTrue(aliceSharkMessage_11.couldBeDecrypted());
+        Assert.assertTrue(aliceSharkMessage_11.encrypted());
+        Assert.assertTrue(aliceSharkMessage_11.verified());
+        // message received by Alice from Bob?
+        Assert.assertTrue(claraPeer.samePeer(aliceSharkMessage_11.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage aliceSortedMessage_11 = SortedMessageImpl.byteArrayToSortedMessage(aliceSharkMessage_11.getContent());
+        // Add the received message to the factory
+        aliceSortedMessageFactory.addIncomingSortedMessage(aliceSortedMessage_11);
+        // Check sortedMessage id
+        Assert.assertEquals(claraSortedMessage_1.getID(), aliceSortedMessage_11.getID());
+
+        Assert.assertTrue(Utils.compareArrays(aliceSortedMessage_11.getContent(), MESSAGE_BYTE));
+
+        // SortedMessage parents should be ["id"] bobSortedMessage.getID()
+        Assert.assertEquals(1, aliceSortedMessage_11.getParents().size());
+        Assert.assertTrue(aliceSortedMessage_11.getParents().contains(claraSortedMessage.getID()));
+
+        // Alice send a response to Bob
+        SortedMessage aliceSortedMessage_3 = aliceSortedMessageFactory.produceSortedMessage(MESSAGE_3_BYTE, null);
+        aliceMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(aliceSortedMessage_3) , URI, bobPeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Alice - Bob ////////////////////////////////////////////////////
+        this.runEncounter(this.alicePeer, this.bobPeer, true);
+
+        // Test results Bob received message
+        SharkMessage bobSharkMessage_3 = bobChannel.getMessages().getSharkMessage(3, true);
+        Assert.assertTrue(bobSharkMessage_3.couldBeDecrypted());
+        Assert.assertTrue(bobSharkMessage_3.encrypted());
+        Assert.assertTrue(bobSharkMessage_3.verified());
+        // message received by Bob from Alice?
+        Assert.assertTrue(alicePeer.samePeer(bobSharkMessage_3.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage bobSortedMessage_3 = SortedMessageImpl.byteArrayToSortedMessage(bobSharkMessage_3.getContent());
+        // Add the received message to the factory
+        bobSortedMessageFactory.addIncomingSortedMessage(bobSortedMessage_3);
+        // Check sortedMessage id
+        Assert.assertEquals(aliceSortedMessage_3.getID(), bobSortedMessage_3.getID());
+
+        Assert.assertTrue(Utils.compareArrays(bobSortedMessage_3.getContent(), MESSAGE_3_BYTE));
+        // SortedMessage parents should be ["id1", "id2"] aliceSortedMessage_1.getID(), aliceSortedMessage_11.getID()
+        Set<CharSequence> aliceSortedMessage_3_parents = new HashSet<>();
+        aliceSortedMessage_3_parents.add(aliceSortedMessage_1.getID());
+        aliceSortedMessage_3_parents.add(aliceSortedMessage_11.getID());
+
+        Assert.assertEquals(2, bobSortedMessage_3.getParents().size());
+        Assert.assertTrue(bobSortedMessage_3.getParents().containsAll(aliceSortedMessage_3_parents));
+
+        // Alice Send a response to Clara
+        aliceMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(aliceSortedMessage_3) , URI, claraPeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter Alice - Clara ////////////////////////////////////////////////////
+        this.runEncounter(this.alicePeer, this.claraPeer, true);
+
+        // Test results Clara received message
+        SharkMessage claraSharkMessage_3 = claraChannel.getMessages().getSharkMessage(3, true);
+        Assert.assertTrue(claraSharkMessage_3.couldBeDecrypted());
+        Assert.assertTrue(claraSharkMessage_3.encrypted());
+        Assert.assertTrue(claraSharkMessage_3.verified());
+        // message received by Bob from Alice?
+        Assert.assertTrue(alicePeer.samePeer(claraSharkMessage_3.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage claraSortedMessage_3 = SortedMessageImpl.byteArrayToSortedMessage(claraSharkMessage_3.getContent());
+        // Add the received message to the factory
+        bobSortedMessageFactory.addIncomingSortedMessage(claraSortedMessage_3);
+        // Check sortedMessage id
+        Assert.assertEquals(aliceSortedMessage_3.getID(), claraSortedMessage_3.getID());
+
+        Assert.assertTrue(Utils.compareArrays(claraSortedMessage_3.getContent(), MESSAGE_3_BYTE));
+        // SortedMessage parents should be ["id1", "id2"] aliceSortedMessage_1.getID(), aliceSortedMessage_11.getID()
+        Assert.assertEquals(2, claraSortedMessage_3.getParents().size());
+        Assert.assertTrue(claraSortedMessage_3.getParents().containsAll(aliceSortedMessage_3_parents));
+
+        // David came later in the chat and got only the last message from Alice
+        // David answer to Alice
+        // David send a response to Alice
+        SortedMessage davidSortedMessage_4 = aliceSortedMessageFactory.produceSortedMessage(MESSAGE_4_BYTE, null);
+        davidMessenger.sendSharkMessage(SortedMessageImpl.sortedMessageByteArray(davidSortedMessage_4) , URI, alicePeer.getPeerID(), true, true);
+
+        ///////////////////////////////// Encounter David - Alice ////////////////////////////////////////////////////
+        this.runEncounter(this.davidPeer, this.alicePeer, true);
+
+        // Test results Alice received message
+        SharkMessage aliceSharkMessage_4 = aliceChannel.getMessages().getSharkMessage(0, true);
+        Assert.assertTrue(aliceSharkMessage_4.couldBeDecrypted());
+        Assert.assertTrue(aliceSharkMessage_4.encrypted());
+        Assert.assertTrue(aliceSharkMessage_4.verified());
+        // message received by Alice from David?
+        Assert.assertTrue(davidPeer.samePeer(aliceSharkMessage_4.getSender()));
+        // Convert received bytes to SortedMessage
+        SortedMessage aliceSortedMessage_4 = SortedMessageImpl.byteArrayToSortedMessage(aliceSharkMessage_4.getContent());
+        // Add the received message to the factory
+        davidSortedMessageFactory.addIncomingSortedMessage(aliceSortedMessage_4);
+        // Check sortedMessage id
+        Assert.assertEquals(davidSortedMessage_4.getID(), aliceSortedMessage_4.getID());
+        Assert.assertTrue(Utils.compareArrays(aliceSortedMessage_4.getContent(), MESSAGE_4_BYTE));
+        // SortedMessage parents should be ["id"] aliceSortedMessage_3.getID()
+        Assert.assertEquals(1, aliceSortedMessage_4.getParents().size());
+        Assert.assertTrue(aliceSortedMessage_4.getParents().contains(aliceSortedMessage_3.getID()));
     }
 }
