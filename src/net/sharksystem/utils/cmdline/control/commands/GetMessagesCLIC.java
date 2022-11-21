3d2076e341cabb39de78fce4791e692e6d1dc149
Clemens <cle.zander@gmail.com>
2022-11-21 11:48:26 +0100

    first implementation of get messages command

---
+++ src/net/sharksystem/utils/cmdline/control/commands/GetMessagesCLIC.java
@@ -0,0 +1,51 @@
+package net.sharksystem.utils.cmdline.control.commands;
+
+import net.sharksystem.SharkException;
+import net.sharksystem.messenger.SharkMessage;
+import net.sharksystem.messenger.SharkMessageList;
+import net.sharksystem.messenger.SharkMessengerChannel;
+import net.sharksystem.messenger.SharkMessengerComponent;
+import net.sharksystem.utils.cmdline.model.CLIModelInterface;
+import net.sharksystem.utils.cmdline.view.CLIInterface;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class GetMessagesCLIC extends CLICommand {
+
+    public GetMessagesCLIC(String identifier, boolean rememberCommand) {
+        super(identifier, rememberCommand);
+    }
+
+    @Override
+    public void execute(CLIInterface ui, CLIModelInterface model, List<String> args) {
+        try {
+            SharkMessengerComponent messenger = model.getMessengerFromPeer(args.get(0));
+            SharkMessengerChannel channel = messenger.getChannel(args.get(1));
+            SharkMessageList list = channel.getMessages();
+
+            for (int i = 0; i < list.size(); i++) {
+                SharkMessage m = list.getSharkMessage(i, true);
+                ui.printInfo(new String(m.getContent(), StandardCharsets.UTF_8));
+            }
+
+        } catch (SharkException | IOException e) {
+            ui.printError(e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Returns one or more messages a peer received.");
+        return sb.toString();
+    }
+
+    @Override
+    public String getDetailedDescription() {
+        return this.getDescription();
+        //TODO: add detailed description
+    }
+}
