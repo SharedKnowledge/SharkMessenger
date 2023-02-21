fc2bb796c7743dd182233315e66b289a9ae28b5c
thsc42 <thomas.schwotzer@htw-berlin.de>
2023-02-21 09:13:22 +0100

    works with new ASAPJava release. Some bugs came up, though. Working on it.

--- src/net/sharksystem/messenger/SharkMessengerComponentFactory.java
+++ src/net/sharksystem/messenger/SharkMessengerComponentFactory.java
@@ -2,22 +2,24 @@ package net.sharksystem.messenger;
 
 import net.sharksystem.SharkComponent;
 import net.sharksystem.SharkComponentFactory;
-import net.sharksystem.contactinformation.SharkContactInformationComponent;
 import net.sharksystem.pki.SharkPKIComponent;
 
 public class SharkMessengerComponentFactory implements SharkComponentFactory {
     private final SharkPKIComponent pkiComponent;
-    private final SharkContactInformationComponent contactsComponent;
+//    private final SharkContactInformationComponent contactsComponent;
 
+    /*
     public SharkMessengerComponentFactory(
             SharkPKIComponent pkiComponent, SharkContactInformationComponent contactsComponent) {
 
         this.pkiComponent = pkiComponent;
         this.contactsComponent = contactsComponent;
     }
+     */
 
     public SharkMessengerComponentFactory(SharkPKIComponent pkiComponent) {
-        this(pkiComponent, null);
+//        this(pkiComponent, null);
+        this.pkiComponent = pkiComponent;
     }
 
     @Override
