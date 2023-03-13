50b0f1a50f81ea137db19ea08ad4f7136ed69f37
Clemens <cle.zander@gmail.com>
2023-03-13 20:58:22 +0100

    Identity Assurance of known peers can now be viewed using the corresponding command.

--- src/net/sharksystem/utils/cmdline/control/commands/pki/CLICGetIdentityAssurance.java
+++ src/net/sharksystem/utils/cmdline/control/commands/pki/CLICGetIdentityAssurance.java
@@ -1,23 +1,45 @@
 package net.sharksystem.utils.cmdline.control.commands.pki;
 
+import net.sharksystem.pki.SharkPKIComponent;
+import net.sharksystem.utils.cmdline.control.CLICQuestionnaireBuilder;
+import net.sharksystem.utils.cmdline.control.CLICSharkPeerArgument;
 import net.sharksystem.utils.cmdline.control.CLICommand;
 import net.sharksystem.utils.cmdline.control.CLICQuestionnaire;
 import net.sharksystem.utils.cmdline.model.CLIModelInterface;
 import net.sharksystem.utils.cmdline.view.CLIInterface;
 
 public class CLICGetIdentityAssurance extends CLICommand {
+
+    private final CLICSharkPeerArgument owner;
+
+    private final CLICSharkPeerArgument subject;
+
     public CLICGetIdentityAssurance(String identifier, boolean rememberCommand) {
         super(identifier, rememberCommand);
+        this.owner = new CLICSharkPeerArgument();
+        this.subject = new CLICSharkPeerArgument();
     }
 
     @Override
     public CLICQuestionnaire specifyCommandStructure() {
-        return null;
+        return new CLICQuestionnaireBuilder().
+                addQuestion("Owner peer name: ", this.owner).
+                addQuestion("Subject peer name: ", this.subject).
+                build();
     }
 
     @Override
     public void execute(CLIInterface ui, CLIModelInterface model) throws Exception {
+        SharkPKIComponent pki = model.getPKIFromPeer(this.owner.getValue());
+        int iA = pki.getIdentityAssurance(this.subject.getValue().getPeerID());
 
+        StringBuilder sb = new StringBuilder();
+        sb.append("Identity Assurance of ");
+        sb.append(this.subject.getValue().getPeerID());
+        sb.append("is: ");
+        sb.append(iA);
+
+        ui.printInfo(sb.toString());
     }
 
     @Override
