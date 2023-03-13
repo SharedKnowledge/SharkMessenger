593d1cc5c2cd4599a17d0de60103215afc981d04
Clemens <cle.zander@gmail.com>
2023-03-13 20:36:08 +0100

    Get RSA key pair creation time command implemented

--- src/net/sharksystem/utils/cmdline/control/commands/pki/CLICGetKeysCreationTime.java
+++ src/net/sharksystem/utils/cmdline/control/commands/pki/CLICGetKeysCreationTime.java
@@ -1,23 +1,36 @@
 package net.sharksystem.utils.cmdline.control.commands.pki;
 
+import net.sharksystem.asap.utils.DateTimeHelper;
+import net.sharksystem.pki.SharkPKIComponent;
+import net.sharksystem.utils.cmdline.control.CLICQuestionnaireBuilder;
+import net.sharksystem.utils.cmdline.control.CLICSharkPeerArgument;
 import net.sharksystem.utils.cmdline.control.CLICommand;
 import net.sharksystem.utils.cmdline.control.CLICQuestionnaire;
 import net.sharksystem.utils.cmdline.model.CLIModelInterface;
 import net.sharksystem.utils.cmdline.view.CLIInterface;
 
 public class CLICGetKeysCreationTime extends CLICommand {
+
+    private final CLICSharkPeerArgument peer;
+
     public CLICGetKeysCreationTime(String identifier, boolean rememberCommand) {
         super(identifier, rememberCommand);
+        this.peer = new CLICSharkPeerArgument();
     }
 
     @Override
     public CLICQuestionnaire specifyCommandStructure() {
-        return null;
+        return new CLICQuestionnaireBuilder().
+                addQuestion("Peer name: ", this.peer).
+                build();
     }
 
     @Override
     public void execute(CLIInterface ui, CLIModelInterface model) throws Exception {
+        SharkPKIComponent pki = (SharkPKIComponent) this.peer.getValue().getComponent(SharkPKIComponent.class);
+        String creationTime = DateTimeHelper.long2DateString(pki.getKeysCreationTime());
 
+        ui.printInfo("RSA key pairs were created at: " + creationTime);
     }
 
     @Override
