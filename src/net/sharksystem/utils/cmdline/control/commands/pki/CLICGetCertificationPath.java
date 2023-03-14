53ecc220c3e4549712fb32a23db7e4fcc11a7b62
Clemens <cle.zander@gmail.com>
2023-03-14 21:17:53 +0100

    The path of a certificate for a specific peer can now be displayed by invoking the corresponding command.

--- src/net/sharksystem/utils/cmdline/control/commands/pki/CLICGetCertificationPath.java
+++ src/net/sharksystem/utils/cmdline/control/commands/pki/CLICGetCertificationPath.java
@@ -1,23 +1,42 @@
 package net.sharksystem.utils.cmdline.control.commands.pki;
 
+import net.sharksystem.pki.SharkPKIComponent;
+import net.sharksystem.utils.cmdline.control.CLICQuestionnaireBuilder;
+import net.sharksystem.utils.cmdline.control.CLICSharkPeerArgument;
 import net.sharksystem.utils.cmdline.control.CLICommand;
 import net.sharksystem.utils.cmdline.control.CLICQuestionnaire;
 import net.sharksystem.utils.cmdline.model.CLIModelInterface;
 import net.sharksystem.utils.cmdline.view.CLIInterface;
 
+import java.util.List;
+
 public class CLICGetCertificationPath extends CLICommand {
+
+    private final CLICSharkPeerArgument subject;
+
     public CLICGetCertificationPath(String identifier, boolean rememberCommand) {
         super(identifier, rememberCommand);
+        this.subject = new CLICSharkPeerArgument();
     }
 
     @Override
     public CLICQuestionnaire specifyCommandStructure() {
-        return null;
+        return new CLICQuestionnaireBuilder().
+                addQuestion("Subject name: ", this.subject).
+                build();
     }
 
     @Override
     public void execute(CLIInterface ui, CLIModelInterface model) throws Exception {
+        SharkPKIComponent pki = model.getPKIFromPeer(this.subject.getValue());
+        List<CharSequence> path = pki.getIdentityAssurancesCertificationPath(this.subject.getValue().getPeerID());
 
+        StringBuilder sb = new StringBuilder();
+        for(CharSequence userID : path) {
+            sb.append(" -> ");
+            sb.append(userID);
+        }
+        ui.printInfo(sb.toString());
     }
 
     @Override
